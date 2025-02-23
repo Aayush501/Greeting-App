b74e0b6328749cb7cb85e0b950e4d16a7663ea4d
Aayush501 <shrivastavashu501@gmail.com>
2025-02-23 22:08:46 +0530

    [Aayush Kumar Shrivastav] Add . Using GreetingController return JSON for different HTTP Methods -> Solution

---
+++ src/main/java/com/greeting/restapis/controllers/GreetingController.java
@@ -0,0 +1,29 @@
+package com.greeting.restapis.controllers;
+
+import com.greeting.restapis.entities.Greeting;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public Greeting getGreeting() {
+        return new Greeting("Hello Ankit Rajput, this is a GET request!", HttpStatus.OK.value());
+    }
+
+    @PostMapping
+    public Greeting postGreeting() {
+        return new Greeting("Hello Ankit Rajput, this is a POST request!", HttpStatus.CREATED.value());
+    }
+
+    @PutMapping
+    public Greeting putGreeting() {
+        return new Greeting("Hello Ankit Rajput, this is a PUT request!", HttpStatus.OK.value());
+    }
+
+    @DeleteMapping
+    public Greeting deleteGreeting() {
+        return new Greeting("Hello Ankit Rajput, this is a DELETE request!", HttpStatus.NO_CONTENT.value());
+    }
+}
