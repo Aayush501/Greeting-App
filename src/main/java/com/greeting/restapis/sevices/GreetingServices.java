e55faf8f7ed76eafb7f59d8952dc582c936606e7
Aayush501 <shrivastavashu501@gmail.com>
2025-02-23 22:57:46 +0530

    [Aayush Kumar Shrivastav] Add . Extend GreetingController to use Services Layer to get Simple Greeting message -> Solution

---
+++ src/main/java/com/greeting/restapis/sevices/GreetingServices.java
@@ -0,0 +1,27 @@
+package com.greeting.restapis.sevices;
+
+import org.springframework.stereotype.Service;
+
+// Service layer to handle business logic
+@Service
+public class GreetingServices {
+    // Get method to get the content
+    public String getGreetingMessage() {
+        return "Hello Aayush Kumar, this is a GET request!";
+    }
+
+    // Post Method to send the response body
+    public String postGreetingMessage() {
+        return "Hello Aayush Kumar, this is a POST request!";
+    }
+
+    // Put Method to  update the data
+    public String putGreetingMessage() {
+        return "Hello Aayush Kumar, this is a PUT request!";
+    }
+
+    // Delete method to delete the data
+    public String deleteGreetingMessage() {
+        return "Hello Aayush Kumar, this is a DELETE request!";
+    }
+}
